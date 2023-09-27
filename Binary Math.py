248d60e1050e2baaa1cb66ba47a0e227c9e702b2
MrAwesome7711 <111150891+MrAwesome7711@users.noreply.github.com>
2023-09-26 22:06:40 -0500

    Version 1.1
    Added an experimental addition menu for testing a more efficient style (not yet added to main code line)

--- Binary Math.py
+++ Binary Math.py
@@ -110,6 +110,12 @@ def twoAdd():
     numOne = str(input(YELLOW + "Enter your first number: " + RED))
     numTwo = str(input(YELLOW + "Enter your second number: " + RED))
 
+    # Normalize lengths (if not the same)
+    if (len(numOne) < len(numTwo)):
+        numOne = normalize(numOne, len(numTwo))
+    if (len(numTwo) < len(numOne)):
+        numTwo = normalize(numTwo, len(numOne))
+
     # Add
     subAnswer = oneAddSub(numOne, numTwo, "answer")
 
@@ -119,7 +125,42 @@ def twoAdd():
         print(GREEN + "\nAnswer: " + MAGENTA + subAnswer + WHITE)
     elif oneAddSub(numOne, numTwo, "buffer")[len(numOne)] == "0":
         print(GREEN + "\nAnswer: " + MAGENTA + subAnswer + WHITE)
-      
+
+
+
+# Color Menu        
+def colorMenu():
+    print("Color Menu")
+    print("[1] Black")
+    print("[2] Red")
+    print("[3] Green")
+    print("[4] Blue")
+    print("[5] Magenta")
+    print("[6] Cyan")
+    print("[7] White")
+    selection = input(YELLOW + "Select a menu option: " + RED)
+    print(WHITE + "\n\n")
+    if selection == "1":
+        print(BLACK)
+        exit()
+    elif selection == "2":
+        print(RED)
+        exit()
+    elif selection == "3":
+        print(GREEN)
+        exit()
+    elif selection == "4":
+        print(BLUE)
+        exit()
+    elif selection == "5":
+        print(MAGENTA)
+        exit()
+    elif selection == "6":
+        print(CYAN)
+        exit()
+    elif selection == "7":
+        print(WHITE)
+        exit()      
 
 # Additional sub-method for actually adding numbers
 def oneAddSub(numOne, numTwo, returnValue):
@@ -157,7 +198,51 @@ def oneAddSub(numOne, numTwo, returnValue):
     elif returnValue == "buffer":
         return buffer
     
+# Add two binary numbers - New and improved!
+def addTwo(numOne, numTwo, complement):
+    # Variables
+    buffer = "0"
+    answer = ""
+    
+    # Normalize lengths (if not the same)
+    if (len(numOne) < len(numTwo)):
+        numOne = normalize(numOne, len(numTwo))
+    if (len(numTwo) < len(numOne)):
+        numTwo = normalize(numTwo, len(numOne))
 
+    # Main loop
+    for x in range(len(numOne)):
+        # Add a variable that allows going through strings backwards
+        y = len(numOne) - x - 1
+
+        # Take sum of digits in two numbers and buffer
+        sum = int(numOne[y]) + int(numTwo[y]) + int(buffer[x])
+        # if 0, zero in answer and no carry
+        if sum == 0:
+            answer = "0" + answer
+            buffer = buffer + "0"
+        # if 1, one in answer and no carry
+        elif sum == 1:
+            answer = "1" + answer
+            buffer = buffer + "0"
+        # if 2, zero in answer and carry one
+        elif sum == 2:
+            answer = "0" + answer
+            buffer = buffer + "1"
+        # if 3, one in answer and carry one
+        elif sum == 3:
+            answer = "1" + answer
+            buffer = buffer + "1"
+    
+    # Carry handling for each complement
+    if buffer[len(numOne)] == "1":
+        if complement == "one":
+            return addTwo(answer, normalize("1", answer), "one")
+        if complement == "two":
+            return "1" + answer
+    if buffer[len(numOne)] == "0":
+        return answer
+   
 # Additional sub-method to normalize a number to a given length by adding zeros
 def normalize(number, length):
     while len(number) < length:
@@ -173,8 +258,10 @@ def devMenu():
         print("Please note that these features are temporary and/or developmental, so don't expect them to work!")
         print("Also sorry no color but this is dev menu so deal with it")
         print("[1] normalize binary number")
-        print("[2] Base binary addition method")
+        print("[2] NEW Binary Addition")
         print("[3] Color Menu")
+        print("[4] Endless Addition")
+        print("[5] Multiply Binary Numbers")
         print("[0] Return to previous menu\n")
         selection = input("Select and option: ")
         print("\n")
@@ -186,53 +273,21 @@ def devMenu():
         elif selection == "2":
             number1 = str(input("Enter a number: "))
             number2 = str(input("Enter another number: "))
-            argument1 = str(input("Do you want answer or buffer?: "))
-            print("Answer: " + oneAddSub(number1, number2, argument1))
+            argument1 = str(input("one or two complement?: "))
+            print("Answer: " + addTwo(number1, number2, argument1))
             print(newLine)
         elif selection == "3":
             colorMenu()
-#        elif selection == "4":
-#            print(newLine)
+        elif selection == "4":
+            print("Placeholder message here :)")
+            print(newLine)
+        elif selection == "5":
+            print("Placeholder message here :)")
+            print(newLine)
         elif selection == "0":
             break
         else:
             print("invalid input" + newLine)
-
-# Color Menu        
-def colorMenu():
-    print("Color Menu")
-    print("[1] Black")
-    print("[2] Red")
-    print("[3] Green")
-    print("[4] Blue")
-    print("[5] Magenta")
-    print("[6] Cyan")
-    print("[7] White")
-    selection = input(YELLOW + "Select a menu option: " + RED)
-    print(WHITE + "\n\n")
-    if selection == "1":
-        print(BLACK)
-        exit()
-    elif selection == "2":
-        print(RED)
-        exit()
-    elif selection == "3":
-        print(GREEN)
-        exit()
-    elif selection == "4":
-        print(BLUE)
-        exit()
-    elif selection == "5":
-        print(MAGENTA)
-        exit()
-    elif selection == "6":
-        print(CYAN)
-        exit()
-    elif selection == "7":
-        print(WHITE)
-        exit()
-
-
 
 
 
