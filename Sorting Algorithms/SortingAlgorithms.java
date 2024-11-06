a84619ff4606f416dad85381df9aee823c0185e7
MrAwesome7711 <111150891+MrAwesome7711@users.noreply.github.com>
2024-11-06 13:24:46 -0600

    Sorting algorithms

---
+++ Sorting Algorithms/SortingAlgorithms.java
@@ -0,0 +1,12 @@
+public class SortingAlgorithms<E extends Comparable<E>>
+{
+    private E[] CustomSort(E[] startArray)
+    {
+        E[] tempArray = (E[]) new Comparable[(startArray.length*2)-1];
+        E[] finalArray = (E[]) new Comparable[startArray.length];
+
+        
+
+        return finalArray;
+    }
+}
